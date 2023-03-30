22c7b275edb88697465826400579c3509faec4c1
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2023-03-30 15:36:12 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RouteTest from "./components/RouteTest";
+
+import Home from "./pages/Home";
+import New from "./pages/new";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new" element={<New />}></Route>
+          <Route path="/Edit" element={<Edit />}></Route>
+          <Route path="/Diary/:id" element={<Diary />}></Route>
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 

57d64dfc2538772139ebdfa039720c3b22f35291
Lumire-ee <58719969+Lumire-ee@users.noreply.github.com>
2023-08-02 14:43:00 +0900

    23-08-02

--- src/App.js
+++ src/App.js
@@ -37,8 +37,41 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "오늘의 일기 1번",
+    date: 1690953186154,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의 일기 2번",
+    date: 1690953186155,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "오늘의 일기 3번",
+    date: 1690953186156,
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "오늘의 일기 4번",
+    date: 1690953186157,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "오늘의 일기 5번",
+    date: 1690953186158,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
 
@@ -78,7 +111,7 @@ function App() {
   };
 
   return (
-    <DiaryStateContext.Provider>
+    <DiaryStateContext.Provider value={data}>
       <DiaryDispatchContext.Provider
         value={{
           onCreate,
---
+++ src/components/DiaryList.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "lastest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+const DiaryList = ({ diaryList }) => {
+  // 정렬
+  const [sortType, setSortType] = useState("lastest");
+
+  const getProcessDiaryList = () => {};
+
+  return (
+    <div>
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {diaryList.map((it) => (
+        <div key={it.id}> {it.content}</div>
+      ))}
+    </div>
+  );
+};
+
+DiaryList.defaultProps = {
+  diaryList: [],
+};
+
+export default DiaryList;
